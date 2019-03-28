9dfc791a4b83d20f84b4bd336e0f5d04bf786e3f
romainnovationgroup <romakokoshinnovation@gmail.com>
2019-03-28 18:28:39 +0200

    transfer

--- app/src/main/java/com/citywork/utils/ChartUtils.java
+++ app/src/main/java/com/citywork/utils/ChartUtils.java
@@ -1,19 +1,46 @@
 package com.citywork.utils;
 
+import com.citywork.model.db.models.Building;
 import com.citywork.model.db.models.City;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class ChartUtils {
 
-    private ArrayList<IBarDataSet> getDataForday(City city) {
+    private final int dayDivider = 4;
+
+    private ArrayList<IBarDataSet> getDataForday(@NotNull City city) {
         ArrayList<BarEntry> values = new ArrayList<>();
 
-        //TODO: CREATE LIST OF DATA
+        Date date;// given date
+        Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+
+        int curhour;
+
+        for (Building building : city.getBuildings()) {
+            date = new Date(building.getPomodoro().getStarttime());
+            calendar.setTime(date);
+
+            curhour = calendar.get(Calendar.HOUR_OF_DAY);
+
+            for (int i = dayDivider; i < 24; i += dayDivider) {
+                curhour -= dayDivider;
+                if (curhour > dayDivider) {
+                    continue;
+                } else {
+                    values.add(new BarEntry(i, calendar.get(Calendar.HOUR_OF_DAY)));
+                }
+            }
+        }
 
         BarDataSet set1;
 
